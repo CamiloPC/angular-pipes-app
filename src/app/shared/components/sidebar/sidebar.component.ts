cd8d49e204313067e65f3c7146e242484ee392c5
CamiloPC <73408040+CamiloPC@users.noreply.github.com>
2024-03-18 12:27:27 -0700

    Fin de Sección 7

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shared-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+
+  constructor ( private gifsService: GifsService ) {
+
+  }
+
+
+  get tags() {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag ( tag: string ): void {
+    this.gifsService.searchTag( tag );
+  }
+}
